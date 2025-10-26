706d4119317c186df1397f5ec0c15b9f2b5393a6
wuying-0824 <wu_ying24@163.com>
2025-10-26 07:41:26 +0000

    添加investment.py

---
+++ investment.py
@@ -0,0 +1,10 @@
+amount = float(input("Enter amount: "))
+inrate = float(input("Enter Interest rate: "))
+period = int(input("Enter period: "))
+value = 0
+year = 1
+while year <= period:
+    value = amount + (inrate * amount)
+    print("Year {} Rs. {:.2f}".format(year, value))
+    amount = value
+    year = year+1
---
+++ testhundrred.py
@@ -0,0 +1,7 @@
+number = int(input("pls enter an integer"))
+if number < 100:
+    print("the number is smaller than 100")
+elif number == 100:
+    print("the number is 100")
+else:
+    print("the number is greater than 100")
